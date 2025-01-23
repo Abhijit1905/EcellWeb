967d6c5f65d824fe8ada4a5681152acf656577b5
Abhijit Sahu <jagjeete19@gmail.com>
2025-01-23 17:24:46 +0530

    Add files via upload

--- src/Second.js
+++ src/Second.js
@@ -13,7 +13,7 @@ export default function Second() {
                 </div>
                 <div className="flex intro gap-9 justify-center align-center">
                     <div className='circle'><img src='abhijit.png'></img></div>
-                    <div className='introo flex-col text-center'>
+                    <div className='introo text-center gap-3'>
                         <h3>Abhijit Sahu</h3>
                         <h3>Frontend Developer</h3>
                         <h3>20236005</h3>
